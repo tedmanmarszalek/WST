92a3db86016ea048b0d0f5188ce3b36dddd125dd
champfox <champion_foxhound@hotmail.com>
2016-04-26 21:34:30 -0500

    Fixed sculpture name change bug

--- routes/Sculptures.js
+++ routes/Sculptures.js
@@ -33,7 +33,8 @@ router.post('/', function(req, res, next){
 					coordinates_longitude: req.body.coordinates_longitude,
 					artist: req.body.artist,
 					artist_statement: req.body.artist_statement, 
-					type: req.body.type
+					type: req.body.type, 
+					number: req.body.number
 				};
 				console.log("Created new sculpture object");
 				var sculpture = new Sculpture(sculpture_object);
@@ -48,7 +49,8 @@ router.post('/', function(req, res, next){
 						coordinates_longitude: req.body.coordinates_longitude,
 						artist: req.body.artist,
 						artist_statement: req.body.artist_statement, 
-						type: req.body.type
+						type: req.body.type,
+						number: req.body.number
 						}
 					};
 				var options = {upsert: true, new: true};
@@ -60,6 +62,62 @@ router.post('/', function(req, res, next){
 							res.send(err);
 						} else {
 							console.log(result);
+							console.log("Success");
+							res.json({
+								success: true,
+								msg: ""
+							});
+						}
+					}
+				);
+
+	      }
+	    });
+
+	  } else {
+
+	    // if there is no token
+	    // return an error
+	    return res.status(403).send({ 
+	        success: false, 
+	        message: 'No token provided.' 
+	    });
+	  }
+});
+
+router.post('/edit', function(req, res, next){
+	var token = req.body.token;
+	if (token) {
+	    // verifies secret and checks exp
+	    jwt.verify(token, 'UPDSECRET', function(err, decoded) {    
+	      if (err) {
+	        return res.json({ success: false, message: 'Failed to authenticate token.' });    
+	      } else {
+				//Create a new entry if no matching name found, otherwise update existing one
+				console.log("got into edit");
+				var update = {$set: { 
+						sculpture_name: req.body.sculpture_name,
+						active:req.body.active, 
+						video: req.body.video,
+						image: req.body.image,
+						audio: req.body.audio,
+						coordinates_latitude: req.body.coordinates_latitude,
+						coordinates_longitude: req.body.coordinates_longitude,
+						artist: req.body.artist,
+						artist_statement: req.body.artist_statement, 
+						type: req.body.type,
+						number: req.body.number
+						}
+					};
+				var options = {upsert: false, new: true};
+
+				Sculpture.findByIdAndUpdate(req.body.selectedId, update, options,
+					function(err, result){
+						if(err){
+							console.log(err);
+							res.send(err);
+						} else {
+							console.log("result is " + result);
 							console.log("Success");
 							res.json({
 								success: true,
