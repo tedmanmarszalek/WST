11f7b3f1ac3db58822f74c42de6e753a2a73607f
Ian <champion_foxhound@hotmail.com>
2016-05-07 13:41:54 -0500

    Fixed login

--- models/Sculptures.js
+++ models/Sculptures.js
@@ -4,9 +4,9 @@ var Schema = mongoose.Schema;
 var SculptureSchema = new Schema({
 	//name: {type: String, unique: true, required: true},
 	sculpture_name: {type: String},
-	video: {type: String},
-	image: {type: String},
-	audio: {type: String},
+	video: {type: String, default: ""},
+	image: {type: String, default: ""},
+	audio: {type: String, default: ""},
 	active: {type: Boolean},
 	coordinates_latitude: {type: Number},
 	coordinates_longitude: {type: Number},
